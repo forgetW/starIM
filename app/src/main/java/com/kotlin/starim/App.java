2d13873c51c51811905c55bb1fd15e3f6564431a
17723555741 <503425930@qq.com>
2022-03-08 16:59:23 +0800

    tbs1.2.1

--- app/src/main/java/com/kotlin/starim/App.java
+++ app/src/main/java/com/kotlin/starim/App.java
@@ -2,6 +2,8 @@ package com.kotlin.starim;
 
 import android.app.Application;
 
+import com.tencent.smtt.sdk.QbSdk;
+
 
 public class App extends Application {
 
@@ -9,5 +11,12 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //此方法非常耗时，应当开个线程
+                QbSdk.preinstallStaticTbs(getApplicationContext());
+            }
+        }).start();
     }
 }
